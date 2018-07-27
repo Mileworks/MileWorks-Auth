830290cac431716de6829aea9fcc6fd8a42af58a
borrip <borrip0419@gmail.com>
2018-07-27 15:12:06 +0800

    u

--- src/main/java/com/mileworks/authsys/MileWorksAuthApplication.java
+++ src/main/java/com/mileworks/authsys/MileWorksAuthApplication.java
@@ -1,21 +1,14 @@
 package com.mileworks.authsys;
-
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
-import com.mileworks.authsys.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.mileworks.authsys.repository"})
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EntityScan(basePackageClasses = {
 		MileWorksAuthApplication.class,
 		Jsr310JpaConverters.class

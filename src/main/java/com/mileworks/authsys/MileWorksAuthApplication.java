9bc60683ca792cc07cfa074a0e1dad35858ce145
borrip <borrip0419@gmail.com>
2018-07-27 11:31:34 +0800

    fix
    修复Consider defining a bean named 'entityManagerFactory' in your configuration. 问题

--- src/main/java/com/mileworks/authsys/MileWorksAuthApplication.java
+++ src/main/java/com/mileworks/authsys/MileWorksAuthApplication.java
@@ -4,6 +4,7 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import com.mileworks.authsys.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,14 +12,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.mileworks.authsys.repository"})
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EntityScan(basePackageClasses = {
 		MileWorksAuthApplication.class,
 		Jsr310JpaConverters.class
 })
-@ComponentScan(basePackages = "com.mileworks.authsys")
 public class MileWorksAuthApplication {
 	
 	//在不更改机器时区的情况下改变一个特定应用程序的时区
